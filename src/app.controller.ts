833432c2552e1a88d70214eefd3574ec34cc6c41
Garrett Votaw <garrett.votaw@loves.com>
2024-04-18 09:28:34 -0500

    .

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,24 @@
-/* eslint-disable prettier/prettier */
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('menu')
+  getMenu() {
     return this.appService.getHello();
   }
+
+  @Get('menu/:id')
+  getMenuItem(@Param('id') id: string){
+    return this.appService.getMenuItem(id);
+  }
+
+  @Post('menu')
+  addMenuItem(@Body('item') items: string){
+    console.log(items)
+    return this.appService.addMenuItem(items);
+  }
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,21 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private menu: string[] = [];
+
+  getHello() {
+    return this.menu;
+  }
+
+  addMenuItem(items: string) {
+    if (this.menu.includes(items)) {
+      return this.menu;
+    }
+    this.menu.push(items);
+    return this.menu;
+  }
+
+  getMenuItem(item: string) {
+    return this.menu.find((menuItem) => menuItem === item);
   }
 }
